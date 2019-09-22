3bc121b3c8d26303fa7ff63ca4a8054c3d78cd09
Unknown <anshulchaudhary.218@gmail.com>
2019-09-22 19:07:57 +0530

    Login Page
    Login page complete with landing page

--- resources/views/loginpage/login.blade.php
+++ resources/views/loginpage/login.blade.php
@@ -1,1 +1,92 @@
-login
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Employee Assessment and Rating System/Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+<!--===============================================================================================-->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<!--===============================================================================================-->
+	<!-- <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css"> -->
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('css/logincss/animate.css')}}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{asset('css/logincss/hamburgers.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('css/logincss/select2.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('css/logincss/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/logincss/main.css')}}">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-55">
+						Login
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Email">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<span class="lnr lnr-envelope"></span>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<span class="lnr lnr-lock"></span>
+						</span>
+					</div>
+
+					<div class="contact100-form-checkbox m-l-4">
+						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+						<label class="label-checkbox100" for="ckb1">
+							Remember me
+						</label>
+					</div>
+					
+					<div class="container-login100-form-btn p-t-25">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center w-full p-t-42 p-b-22">
+						<span class="txt1">
+							Or login with
+						</span>
+					</div>
+
+					<a href="#" class="btn-google m-b-10">
+						<img src="{{asset('imgs/icon-google.png')}}" alt="GOOGLE">
+						Google
+					</a>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="{{asset('js/customjs/jquery.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('js/loginjs/popper.js')}}"></script>
+	<script src="{{asset('js/customjs/bootstrap.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('js/loginjs/select2.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('js/loginjs/main.js')}}"></script>
+
+</body>
+</html>
