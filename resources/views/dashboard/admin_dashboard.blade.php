711fd74a2e38fd781902c9761f7e64c7f05a34ed
Sparsh Prabhakar <2017.sparsh.prabhakar@ves.ac.in>
2019-09-23 02:41:51 +0530

    logout,dashboard project

---
+++ resources/views/dashboard/admin_dashboard.blade.php
@@ -0,0 +1,77 @@
+@extends('navbars.navbars')
+
+@section('dashboard')
+    
+<!-- Content -->
+<div class="content">
+        <!-- Animated -->
+        <div class="animated fadeIn">
+            <!-- Widgets  -->
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="stat-widget-five">
+                                <div class="stat-icon dib flat-color-1">
+                                    <i class="fa fa-spinner"></i>
+                                </div>
+                                <div class="stat-content">
+                                    <div class="text-left dib">
+                                        <div class="stat-text"><span class="count">23569</span></div>
+                                        <div class="stat-heading">Ongoing Projects</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="stat-widget-five">
+                                <div class="stat-icon dib flat-color-2">
+                                    <i class="fa fa-user"></i>
+                                </div>
+                                <div class="stat-content">
+                                    <div class="text-left dib">
+                                        <div class="stat-text"><span class="count">3435</span></div>
+                                        <div class="stat-heading">Managers</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="stat-widget-five">
+                                <div class="stat-icon dib flat-color-3">
+                                    <i class="fa fa-users"></i>
+                                </div>
+                                <div class="stat-content">
+                                    <div class="text-left dib">
+                                        <div class="stat-text"><span class="count">349</span></div>
+                                        <div class="stat-heading">Employees</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <!-- /Calender Chart Weather -->
+            <!-- Modal - Calendar - Add New Event -->
+            <!-- /#event-modal -->
+            <!-- Modal - Calendar - Add Category -->
+                    </div>
+                </div>
+            </div>
+        <!-- /#add-category -->
+        </div>
+        <!-- .animated -->
+    </div>
+    <!-- /.content -->
+@endsection
+
---
+++ resources/views/projects/projects.blade.php
@@ -0,0 +1,103 @@
+@extends('navbars.navbars')
+
+@section('dashboard')
+<style>
+
+ /* body{
+     background-color: #fff !important;
+ } */
+ #right-panel{
+     background-color: #fff !important;
+ }
+ .container{
+    background-color: #fff !important;
+ }
+.container1{
+   position: relative;
+   /*margin: 100px auto;*/
+   width: 300px;
+   height: 350px;
+   perspective: 1000px;
+   background-color: transparent;
+}
+.projectdetails{
+   position: relative;
+   width:100%;
+   height: 100%;
+   transition: all 0.8s linear;
+   transform-style: preserve-3d;
+}
+.container1 .projectdetails:hover{
+  transform: rotateY(180deg);
+  box-shadow: -5px 5px 5px #555;
+}
+.front, .back {
+   position: absolute;
+   width: 100%;
+   height: 100%;
+   backface-visibility: hidden;
+ }
+ .front{
+   z-index: 2;
+   transform: rotateY(0deg);
+   box-shadow: 5px 5px 5px #555;
+ }
+ .back {
+   display: block;
+   transform: rotateY(-180deg);
+   box-sizing: border-box;
+   padding: 10px;
+   color: white;
+   text-align: center;
+   background-color: #555;
+   backface-visibility: hidden;
+   transform: rotateY(180deg);
+   text-align: center;
+   line-height: 31px;
+   font-family: 'Titillium Web', sans-serif;
+}
+
+a:link, a:visited ,a{
+   color: inherit;
+}
+
+.front-face{
+   margin-left: 10%;
+   margin-top: 20%
+}
+
+</style>
+<h1 style="background-color: #fff ; margin-top: 8%;margin-left:3%">Ongoing Projects</h1>
+<!-- /#header -->                   
+             <div class="container1" style="margin-top: 2%;margin-left: 10%;">
+               <div class="card projectdetails" style="background-color:rgba(28,252,3,0.6)">
+                 <div class="front face">
+                    <h1>Project Name</h1>
+                    <h3>Department</h3>
+                 </div>
+                 <div class="back face">
+                    <h3>Team Leader</h3>
+                    <h3>Number of employees</h3>
+                 </div>
+
+            </div>
+           </div> 
+          <h1 style="background-color: #fff ; margin-top: 8%;margin-left:3%">Completed Projects</h1>
+<!-- /#header --> 
+            
+              
+           <div class="container1" style="background-color: #fff;margin-top: 2%;margin-left: 10%">                  
+               <div class="card projectdetails" style="background-color:  rgba(252,127,3,0.6);">
+                 <div class="front face">
+                    <h1>Project Name</h1>
+                    <h3>Department</h3>
+                 </div>
+                 <div class="back face">
+                    <h3>Team Leader</h3>
+                    <h3>Number of employees</h3>
+                 </div>
+
+                </div>    
+           </div>  
+
+@endsection
