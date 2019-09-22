c6eb3667a53594d748a15ac3f5aaa75bccd8a67b
Unknown <anshulchaudhary.218@gmail.com>
2019-09-22 19:19:05 +0530

    font
    font changed

--- resources/views/loginpage/login.blade.php
+++ resources/views/loginpage/login.blade.php
@@ -9,7 +9,7 @@
 <!--===============================================================================================-->
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <!--===============================================================================================-->
-	<!-- <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css"> -->
+	<link rel="stylesheet" type="text/css" href="{{asset('fonts/loginfonts/icon-font.min.css')}}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('css/logincss/animate.css')}}">
 <!--===============================================================================================-->	
